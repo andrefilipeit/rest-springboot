f75f314e47552229275adfc303bc85c64528f356
Andre Filipe <andre.filipe.it@gmail.com>
2023-09-18 17:24:41 -0300

    Cross-Origin Resource Sharing (CORS) Implementation

---
+++ CORS/rest-springboot/src/main/java/br/com/rest/springboot/controllers/PersonController.java
@@ -0,0 +1,119 @@
+package br.com.rest.springboot.controllers;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rest.springboot.data.vo.v1.PersonVO;
+import br.com.rest.springboot.services.PersonService;
+import br.com.rest.springboot.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+//@CrossOrigin
+@RestController
+@RequestMapping("/api/person/v1")
+@Tag(name = "Person", description = "Endpoints for Managing People")
+public class PersonController {
+
+	@Autowired //Injection of PerosnService instance
+	private PersonService service;
+	
+	@CrossOrigin(origins = "http://localhost:8080")
+	@GetMapping(value = "/{id}", 
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}) //{required-param}
+	//Operation for Swagger stylization
+		@Operation(summary = "Find a Person", description = "Find a Person", tags = {"Person"}, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+		})
+	//PathVariable is used to recover pathParams explicit in URL - id
+	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
+		
+		return service.findById(id);
+	}
+	
+	@CrossOrigin(origins = {"http://localhost:8080"})
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	//Operation for Swagger stylization
+	@Operation(summary = "Find all Persons", description = "Find all Persons", tags = {"Person"}, responses = {
+		@ApiResponse(description = "Success", responseCode = "200", 
+				content = {
+						@Content(
+								mediaType = "application/json",
+								array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))
+								)
+						}),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+	})
+	public List<PersonVO> findAll() throws Exception {
+		return service.findAll();
+	}
+	
+	@CrossOrigin(origins = {"http://localhost:8080", "https://andrefilipeos.com.br"})
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	@Operation(summary = "Adds a new Person", description = "Adds a new Person by passing in a JSON, XML or YML representation of an Entity", tags = {"Person"}, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+		})
+	public PersonVO create(@RequestBody PersonVO person) throws Exception {
+		return service.create(person);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8080")
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	@Operation(summary = "Updates a Person", description = "Updates a Person by passing in a JSON, XML or YML representation of an Entity", tags = {"Person"}, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+		})
+	public PersonVO update(@RequestBody PersonVO person) throws Exception {
+		return service.update(person);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8080")
+	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Deletes a Person", description = "No Content", tags = {"Person"}, responses = {
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+		})
+	public ResponseEntity<?> delete(@PathVariable(value="id") Long id) throws Exception {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ CORS/rest-springboot/src/main/java/br/com/rest/springboot/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,53 @@
+package br.com.rest.springboot.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.rest.springboot.exceptions.ExceptionResponse;
+import br.com.rest.springboot.exceptions.RequiredObjectIsNullException;
+import br.com.rest.springboot.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice //every time that application throw an exception this comes is ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+	//This method will be used for all exceptions of applications
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions
+		(Exception ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+				ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	//This method will be used for ResourceNotFoundException
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions
+		(Exception ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+				ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(RequiredObjectIsNullException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions
+		(Exception ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+				ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+		
+}
