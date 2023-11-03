1567caab31f3cee97e3c1026643a256bd32a6cc5
Andre Filipe <andre.filipe.it@gmail.com>
2023-11-03 18:22:04 -0300

    Download and upload of single and multiple files

--- JWTAndSpringSecurity/rest-springboot/src/main/java/br/com/rest/springboot/controllers/PersonController.java
+++ JWTAndSpringSecurity/rest-springboot/src/main/java/br/com/rest/springboot/controllers/PersonController.java
@@ -2,7 +2,6 @@ package br.com.rest.springboot.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -170,38 +169,39 @@ public class PersonController {
 	}
 	
 	@PatchMapping(value = "/{id}",
-		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
-	@Operation(summary = "Disable a specific Person by your ID", description = "Disable a specific Person by your ID",
-		tags = {"People"},
-		responses = {
-			@ApiResponse(description = "Success", responseCode = "200",
-				content = @Content(schema = @Schema(implementation = PersonVO.class))
-			),
-			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
+		@Operation(summary = "Disable a specific Person by your ID", description = "Disable a specific Person by your ID",
+			tags = {"People"},
+			responses = {
+				@ApiResponse(description = "Success", responseCode = "200",
+					content = @Content(schema = @Schema(implementation = PersonVO.class))
+				),
+				@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			}
+		)
+		public PersonVO disablePerson(@PathVariable(value = "id") Long id) throws Exception {
+			return service.disablePerson(id);
 		}
-	)
-	public PersonVO disablePerson(@PathVariable(value = "id") Long id) throws Exception {
-		return service.disablePerson(id);
-	}
-	
-	@DeleteMapping(value = "/{id}")
-	@Operation(summary = "Deletes a Person",
-		description = "Deletes a Person by passing in a JSON, XML or YML representation of the person!",
-		tags = {"People"},
-		responses = {
-			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+		
+		@DeleteMapping(value = "/{id}")
+		@Operation(summary = "Deletes a Person",
+			description = "Deletes a Person by passing in a JSON, XML or YML representation of the person!",
+			tags = {"People"},
+			responses = {
+				@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			}
+		)
+		public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+			service.delete(id);
+			return ResponseEntity.noContent().build();
 		}
-	)
-	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
+
 }
