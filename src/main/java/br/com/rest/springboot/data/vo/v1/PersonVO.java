a656ce590f8d3e46a1c634134cab32a550f71147
Andre Filipe <andre.filipe.it@gmail.com>
2023-11-07 12:35:49 -0300

    Support for JSON, XML and X-YAML and Return Customization

--- src/main/java/br/com/rest/springboot/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/rest/springboot/data/vo/v1/PersonVO.java
@@ -2,14 +2,25 @@ package br.com.rest.springboot.data.vo.v1;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
+	
+	//@JsonIgnore (does not display in the request return)
 	private String gender;
 	
 	public PersonVO() {}
