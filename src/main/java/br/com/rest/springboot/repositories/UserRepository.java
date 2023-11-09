a9f4793ddead8d966e29ce40dcbee67adf9040d1
Andre Filipe <andre.filipe.it@gmail.com>
2023-11-09 18:24:18 -0300

    Authentication with JWT and Spring Security

---
+++ src/main/java/br/com/rest/springboot/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package br.com.rest.springboot.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.rest.springboot.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Query("SELECT u FROM User u WHERE u.userName =:userName")
+	User findByUsername(@Param("userName") String userName);
+}
